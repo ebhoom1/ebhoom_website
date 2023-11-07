e461029e2f40280c9f0691e9159ce96ae25cf5bb
“Mazin <mazin@chargemod.com>
2023-11-07 12:11:58 +0530

    image-change

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -60,7 +60,7 @@ export default class Products extends Component {
               </div>
 
               <div className="col-12 col-lg-6 text-center">
-                <img className="wms-img w-75 " src="public/assets/images/ebhoom-flyer.jpg" />
+                <img className="wms-img w-75 " src="assets/images/ebhoom-flyer.jpg" />
               </div>
             </div>
           </div>
